c4e1ddceeea8a73c69fc90a98e3b0888a7a7252f
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-08-30 17:47:45 +0530

    menu-service controller and gateway config

---
+++ menu-service/src/main/java/com/devx/menu_service/controller/MenuController.java
@@ -0,0 +1,30 @@
+package com.devx.menu_service;
+
+import com.devx.menu_service.model.MenuItem;
+import com.devx.menu_service.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Flux<MenuItem>> getAll() {
+        return menuService.getAllMenuItems();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Mono<MenuItem>> add(@RequestBody MenuItem menuItem) {
+        return menuService.addMenuItem(menuItem);
+    }
+}
