121b61215f29c24f17f38d7916d8052ef8a51c85
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-03 19:20:19 +0530

    modified code with integration layer

--- booking-service/src/main/java/com/devx/booking_service/service/RoomTypeService.java
+++ booking-service/src/main/java/com/devx/booking_service/service/RoomTypeService.java
@@ -1,10 +1,12 @@
 package com.devx.booking_service.service;
 
+import com.devx.booking_service.dto.RoomTypeDto;
 import com.devx.booking_service.exception.KeyNotFoundException;
 import com.devx.booking_service.model.RoomType;
 import com.devx.booking_service.record.RoomCountByRoomType;
 import com.devx.booking_service.repository.RoomCountByRoomTypeRepository;
 import com.devx.booking_service.repository.RoomTypeRepository;
+import com.devx.booking_service.utils.AppUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,60 +22,31 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class RoomTypeService extends BaseService<RoomType> {
+public class RoomTypeService{
 
-    public static Logger LOG = LoggerFactory.getLogger(RoomTypeService.class);
-
-    private final RoomTypeRepository roomTypeRepository;
-
-    private final RoomCountByRoomTypeRepository roomCountByRoomTypeRepository;
+    private final RoomTypeServiceIntegration roomTypeServiceIntegration;
 
     @Autowired
-    public RoomTypeService(RoomTypeRepository roomTypeRepository, @Qualifier("jdbcScheduler") Scheduler jdbcScheduler, RoomCountByRoomTypeRepository roomCountByRoomTypeRepository)
-    {
-        super(jdbcScheduler);
-        this.roomTypeRepository = roomTypeRepository;
-        this.jdbcScheduler = jdbcScheduler;
-        this.roomCountByRoomTypeRepository = roomCountByRoomTypeRepository;
+    public RoomTypeService(RoomTypeServiceIntegration roomTypeServiceIntegration) {
+        this.roomTypeServiceIntegration = roomTypeServiceIntegration;
     }
 
-    @Override
-    public ResponseEntity<Mono<RoomType>> insert(RoomType roomType)
+
+    public Mono<RoomTypeDto> addRoomType(RoomTypeDto roomTypeDto)
     {
-        try {
-            Mono<RoomType> savedMono = Mono.fromCallable(() -> roomTypeRepository.save(roomType)).subscribeOn(jdbcScheduler);
-            return new ResponseEntity<>(savedMono, HttpStatus.CREATED);
-        }
-        catch (Exception e)
-        {
-            LOG.error("Error saving room type");
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        RoomType roomType = AppUtils.RoomTypeUtils.convertDtoToEntity(roomTypeDto);
+        return roomTypeServiceIntegration.addRoomType(roomType);
     }
 
-    @Override
-    public ResponseEntity<Flux<RoomType>> getAll()
+
+    public Flux<RoomTypeDto> getAllRoomTypes()
     {
-        try{
-            Flux<RoomType> allTypes = Mono.fromCallable(roomTypeRepository::findAll).flatMapMany(Flux::fromIterable).subscribeOn(jdbcScheduler);
-            return new ResponseEntity<>(allTypes, HttpStatus.OK);
-        }catch (Exception e){
-            LOG.error("Error fetching room types");
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return roomTypeServiceIntegration.getAllRoomTypes();
     }
 
-    public ResponseEntity<Flux<RoomCountByRoomType>> getAvailableRoomCount(LocalDate checkinDate, LocalDate checkoutDate)
+    public Flux<RoomCountByRoomType> getAvailableRoomCount(LocalDate checkinDate, LocalDate checkoutDate)
     {
-        try{
-            Flux<RoomCountByRoomType> flux = Mono.fromCallable(() -> roomCountByRoomTypeRepository.getAvailableRoomCount(checkinDate, checkoutDate)).flatMapMany(Flux::fromIterable).subscribeOn(jdbcScheduler);
-            return ResponseEntity.ok(flux);
-        }
-        catch (Exception e)
-        {
-            LOG.error(e.toString());
-            return ResponseEntity.internalServerError().build();
-        }
+        return roomTypeServiceIntegration.getAvailableRoomCount(checkinDate, checkoutDate);
     }
 
 }
