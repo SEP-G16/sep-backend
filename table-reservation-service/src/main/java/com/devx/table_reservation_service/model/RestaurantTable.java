aa6b70699a908b870991d765abe86b1b45680b60
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-03 19:26:01 +0530

    added unique property and generation strategy

--- table-reservation-service/src/main/java/com/devx/table_reservation_service/model/RestaurantTable.java
+++ table-reservation-service/src/main/java/com/devx/table_reservation_service/model/RestaurantTable.java
@@ -1,7 +1,6 @@
 package com.devx.table_reservation_service.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RestaurantTable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private Integer tableNo;
     private Integer chairCount;
 }
