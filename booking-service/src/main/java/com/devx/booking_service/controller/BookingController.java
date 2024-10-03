b9785b057112d6f92a6cf2e1c45ca4b4c5708fc2
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-03 19:19:34 +0530

    modified code with integration layer

--- booking-service/src/main/java/com/devx/booking_service/controller/BookingController.java
+++ booking-service/src/main/java/com/devx/booking_service/controller/BookingController.java
@@ -1,7 +1,11 @@
 package com.devx.booking_service.controller;
 
+import com.devx.booking_service.dto.BookingDto;
+import com.devx.booking_service.exception.*;
 import com.devx.booking_service.model.Booking;
 import com.devx.booking_service.service.BookingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,30 +16,52 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/booking")
 public class BookingController {
 
+    public static final Logger LOG = LoggerFactory.getLogger(BookingController.class);
     private final BookingService bookingService;
 
     @Autowired
-    public BookingController(BookingService bookingService)
-    {
+    public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Flux<Booking>> getAllBookings()
-    {
-        return bookingService.getAll();
+    public ResponseEntity<Flux<BookingDto>> getAllBookings() {
+        try {
+            return ResponseEntity.ok().body(bookingService.getAllBookings());
+        } catch (Exception e) {
+            LOG.error("Error occurred while fetching bookings{}", e.getMessage());
+            return ResponseEntity.internalServerError().body(Flux.error(e));
+        }
     }
 
-    //TODO:Create DTO for this
     @PostMapping("/add")
-    public ResponseEntity<Mono<Booking>> addBooking(@RequestBody Booking booking)
-    {
-        return bookingService.insert(booking);
+    public ResponseEntity<Mono<BookingDto>> addBooking(@RequestBody BookingDto bookingDto) {
+        try {
+            return ResponseEntity.created(null).body(bookingService.addBooking(bookingDto));
+        } catch (NullFieldException | NoRoomsSelectedException | RoomCountMismatchException |
+                 RoomTypeNotFoundException | RoomNotFoundException | RoomTypeMismatchException e) {
+            return ResponseEntity.badRequest().body(Mono.error(e));
+        } catch (Exception e) {
+            LOG.error("An unexpected error occurred while adding booking{}", e.getMessage());
+            return ResponseEntity.internalServerError().body(Mono.error(e));
+        }
     }
 
     @DeleteMapping("/remove")
-    public ResponseEntity<Void> removeBooking(@RequestBody Long bookingId)
-    {
-        return bookingService.removeBooking(bookingId);
+    public ResponseEntity<Mono<Void>> removeBooking(@RequestBody Long bookingId) {
+        try{
+            if(bookingId == null)
+            {
+                throw new BadRequestException("Booking Id cannot be null");
+            }
+            return ResponseEntity.ok().body(bookingService.removeBooking(bookingId));
+        } catch (BadRequestException | BookingNotFoundException e)
+        {
+            return ResponseEntity.badRequest().body(Mono.error(e));
+        }
+        catch (Exception e) {
+            LOG.error("An unexpected error occurred while removing booking{}", e.getMessage());
+            return ResponseEntity.internalServerError().body(Mono.error(e));
+        }
     }
 }
