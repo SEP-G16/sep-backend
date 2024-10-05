679311bbc8d318b5fcb2d86053a0faa7b5ac4844
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-05 22:00:08 +0530

    test configuration

--- staff-service/src/test/java/com/devx/staff_service/StaffServiceApplicationTests.java
+++ staff-service/src/test/java/com/devx/staff_service/StaffServiceApplicationTests.java
@@ -1,9 +1,16 @@
 package com.devx.staff_service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration
+@ActiveProfiles(value = "test")
 class StaffServiceApplicationTests {
 
     @Test
