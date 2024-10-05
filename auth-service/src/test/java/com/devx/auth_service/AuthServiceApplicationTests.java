b82e461364a0377cf65dc99804897aae420074fe
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-05 18:27:19 +0530

    edited test configuration

--- auth-service/src/test/java/com/devx/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/devx/auth_service/AuthServiceApplicationTests.java
@@ -1,9 +1,16 @@
 package com.devx.auth_service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ContextConfiguration
+@ActiveProfiles(value= {"test", "dev"})
 class AuthServiceApplicationTests {
 
 	@Test
