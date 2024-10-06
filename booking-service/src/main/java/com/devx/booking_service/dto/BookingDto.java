bd718e50a6a012909b69b3671831d8cada7c7837
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-06 11:47:15 +0530

    modified creation timestamps and added json ignores

--- booking-service/src/main/java/com/devx/booking_service/dto/BookingDto.java
+++ booking-service/src/main/java/com/devx/booking_service/dto/BookingDto.java
@@ -3,6 +3,7 @@ package com.devx.booking_service.dto;
 import com.devx.booking_service.model.Room;
 import com.devx.booking_service.model.RoomType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -19,7 +21,7 @@ public class BookingDto {
     private Long id;
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
     private String customerName;
     private String email;
     private String phoneNo;
@@ -38,16 +40,19 @@ public class BookingDto {
     @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate checkoutDate;
 
+    @JsonIgnore
     public boolean hasNullFields()
     {
         return customerName == null || email == null || phoneNo == null || roomType == null || adultCount == 0 || roomCount == 0 || checkinDate == null || checkoutDate == null;
     }
 
+    @JsonIgnore
     public boolean hasNullOrEmptyRoomList()
     {
         return roomList == null || roomList.isEmpty();
     }
 
+    @JsonIgnore
     public boolean isRoomCountAndListLengthMismatch()
     {
         return roomList.size() != roomCount;
