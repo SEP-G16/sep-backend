7c8cf8973a0322ac69fe850c11ff187eb16e3c66
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-07 18:06:20 +0530

    modified for staff and role separately

--- staff-service/src/main/java/com/devx/staff_service/utils/AppUtils.java
+++ staff-service/src/main/java/com/devx/staff_service/utils/AppUtils.java
@@ -1,42 +1,67 @@
 package com.devx.staff_service.utils;
 
+import com.devx.staff_service.dto.RoleDto;
 import com.devx.staff_service.dto.StaffDto;
 import com.devx.staff_service.exception.NullFieldException;
+import com.devx.staff_service.model.Role;
 import com.devx.staff_service.model.Staff;
 
 public class AppUtils {
     private AppUtils() {
     }
 
-    public static Staff convertStaffDtoToStaff(StaffDto staffDto) {
-        if(staffDto.hasNullFields())
-        {
-            throw new NullFieldException("StaffDto has null fields");
+    public static class StaffUtils{
+        public static Staff convertStaffDtoToStaff(StaffDto staffDto) {
+            Staff staff = new Staff();
+            staff.setId(staffDto.getId());
+            staff.setName(staffDto.getName());
+            staff.setRole(RoleUtils.convertRoleDtoToRole(staffDto.getRole()));
+            staff.setGender(staffDto.getGender());
+            staff.setDateOfBirth(staffDto.getDateOfBirth());
+            staff.setAddress(staffDto.getAddress());
+            staff.setPhoneNumber(staffDto.getPhoneNumber());
+            staff.setEmail(staffDto.getEmail());
+            staff.setCreatedAt(staffDto.getCreatedAt());
+            staff.setUpdatedAt(staffDto.getUpdatedAt());
+            return staff;
         }
-        Staff staff = new Staff();
-        staff.setId(staffDto.getId());
-        staff.setName(staffDto.getName());
-        staff.setRole(staffDto.getRole());
-        staff.setGender(staffDto.getGender());
-        staff.setDateOfBirth(staffDto.getDateOfBirth());
-        staff.setAddress(staffDto.getAddress());
-        staff.setPhoneNumber(staffDto.getPhoneNumber());
-        staff.setEmail(staffDto.getEmail());
-        return staff;
+
+        public static StaffDto convertStaffToStaffDto(Staff staff)
+        {
+
+            StaffDto staffDto = new StaffDto();
+            staffDto.setId(staff.getId());
+            staffDto.setName(staff.getName());
+            staffDto.setRole(RoleUtils.convertRoleToRoleDto(staff.getRole()));
+            staffDto.setGender(staff.getGender());
+            staffDto.setDateOfBirth(staff.getDateOfBirth());
+            staffDto.setAddress(staff.getAddress());
+            staffDto.setPhoneNumber(staff.getPhoneNumber());
+            staffDto.setEmail(staff.getEmail());
+            staffDto.setCreatedAt(staff.getCreatedAt());
+            staffDto.setUpdatedAt(staff.getUpdatedAt());
+            return staffDto;
+        }
     }
 
-    public static StaffDto convertStaffToStaffDto(Staff staff)
-    {
+    public static class RoleUtils{
+        public static Role convertRoleDtoToRole(RoleDto roleDto) {
+            Role role = new Role();
+            role.setId(roleDto.getId());
+            role.setName(roleDto.getName());
+            role.setCreatedAt(roleDto.getCreatedAt());
+            role.setUpdatedAt(roleDto.getUpdatedAt());
+            return role;
+        }
 
-        StaffDto staffDto = new StaffDto();
-        staffDto.setId(staff.getId());
-        staffDto.setName(staff.getName());
-        staffDto.setRole(staff.getRole());
-        staffDto.setGender(staff.getGender());
-        staffDto.setDateOfBirth(staff.getDateOfBirth());
-        staffDto.setAddress(staff.getAddress());
-        staffDto.setPhoneNumber(staff.getPhoneNumber());
-        staffDto.setEmail(staff.getEmail());
-        return staffDto;
+        public static RoleDto convertRoleToRoleDto(Role role)
+        {
+            RoleDto roleDto = new RoleDto();
+            roleDto.setId(role.getId());
+            roleDto.setName(role.getName());
+            roleDto.setCreatedAt(role.getCreatedAt());
+            roleDto.setUpdatedAt(role.getUpdatedAt());
+            return roleDto;
+        }
     }
 }
