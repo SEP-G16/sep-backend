4f4df599f48350ab8dca1768db5a1622b2161291
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 11:56:31 +0530

    modified test to add dynamic property source

--- menu-service/src/test/java/com/devx/menu_service/MenuServiceApplicationTests.java
+++ menu-service/src/test/java/com/devx/menu_service/MenuServiceApplicationTests.java
@@ -5,14 +5,30 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ContextConfiguration
 @ActiveProfiles(value = "test")
+@Testcontainers
 class MenuServiceApplicationTests {
+
+	@Container
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+
+	@DynamicPropertySource
+	static void configureTestProperties(DynamicPropertyRegistry registry){
+		registry.add("spring.datasource.url",() -> mySQLContainer.getJdbcUrl());
+		registry.add("spring.datasource.username",() -> mySQLContainer.getUsername());
+		registry.add("spring.datasource.password",() -> mySQLContainer.getPassword());
+		registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
+	}
 
 	@Test
 	void contextLoads() {
