d6467423e6b137dde034c67f65560b9afbaec577
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 11:54:25 +0530

    removed unnecessary imports and moved business logic to integration layer

--- menu-service/src/main/java/com/devx/menu_service/service/CategoryService.java
+++ menu-service/src/main/java/com/devx/menu_service/service/CategoryService.java
@@ -1,50 +1,31 @@
 package com.devx.menu_service.service;
 
+import com.devx.menu_service.dto.CategoryDto;
 import com.devx.menu_service.model.Category;
-import com.devx.menu_service.repository.CategoryRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.devx.menu_service.service.integration.CategoryServiceIntegration;
+import com.devx.menu_service.utils.AppUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 @Service
 public class CategoryService {
 
-    public static final Logger LOG = LoggerFactory.getLogger(CategoryService.class);
 
-    private final CategoryRepository categoryRepository;
-    private final Scheduler jdbcScheduler;
+    private final CategoryServiceIntegration categoryServiceIntegration;
 
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository,
-                           @Qualifier("jdbcScheduler")
-                           Scheduler jdbcScheduler) {
-        this.categoryRepository = categoryRepository;
-        this.jdbcScheduler = jdbcScheduler;
+    public CategoryService(CategoryServiceIntegration categoryServiceIntegration) {
+        this.categoryServiceIntegration = categoryServiceIntegration;
     }
 
-    public ResponseEntity<Flux<Category>> getAll() {
-        try {
-            return ResponseEntity.ok(Mono.fromCallable(categoryRepository::findAll).flatMapMany(Flux::fromIterable).subscribeOn(jdbcScheduler));
-        }
-        catch (Exception e)
-        {
-            LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+    public Mono<CategoryDto> addCategory(CategoryDto categoryDto) {
+        Category category = AppUtils.CategoryUtils.dtoToEntity(categoryDto);
+        return categoryServiceIntegration.addCategory(category);
     }
 
-    public ResponseEntity<Mono<Category>> addCategory(Category category) {
-        try{
-            return ResponseEntity.ok(Mono.just(categoryRepository.save(category)));
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+    public Flux<CategoryDto> getAllCategories() {
+        return categoryServiceIntegration.getAllCategories();
     }
 }
