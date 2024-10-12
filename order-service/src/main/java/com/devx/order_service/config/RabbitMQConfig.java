f64b34bf3e989d2ddc7766ce115b50ce5364e36f
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-13 00:55:06 +0530

    rabbitmq config

---
+++ order-service/src/main/java/com/devx/order_service/config/RabbitMQConfig.java
@@ -0,0 +1,68 @@
+package com.devx.order_service.config;
+
+import com.devx.order_service.message.MessageReceiver;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public DirectExchange direct() {
+        return new DirectExchange("order.direct");
+    }
+
+    private static class ReceiverConfig {
+
+        @Bean
+        public Queue addMenuItemQueue() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding addMenuItemBinding(DirectExchange direct,
+                                          Queue addMenuItemQueue) {
+            return BindingBuilder.bind(addMenuItemQueue)
+                    .to(direct)
+                    .with("addMenuItem");
+        }
+
+        @Bean
+        public Queue addTableQueue() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding addTableBinding(DirectExchange direct,
+                                                 Queue addTableQueue) {
+            return BindingBuilder.bind(addTableQueue)
+                    .to(direct)
+                    .with("addTable");
+        }
+
+        @Bean
+        public MessageReceiver receiver() {
+            return new MessageReceiver();
+        }
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory)
+    {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
+
