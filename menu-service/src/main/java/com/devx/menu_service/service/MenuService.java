c432f58ea1640b208d4ae5512f2aa2e46177bf5b
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-12 11:56:10 +0530

    removed unused imports and moved business logic to integration layer

--- menu-service/src/main/java/com/devx/menu_service/service/MenuService.java
+++ menu-service/src/main/java/com/devx/menu_service/service/MenuService.java
@@ -1,60 +1,31 @@
 package com.devx.menu_service.service;
 
-import com.devx.menu_service.dto.request.AddMenuItemRequestBody;
-import com.devx.menu_service.exception.BadRequestException;
+import com.devx.menu_service.dto.MenuItemDto;
 import com.devx.menu_service.model.MenuItem;
-import com.devx.menu_service.repository.AddOnRepository;
-import com.devx.menu_service.repository.CategoryRepository;
-import com.devx.menu_service.repository.MenuItemRepository;
+import com.devx.menu_service.service.integration.MenuServiceIntegration;
 import com.devx.menu_service.utils.AppUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 @Service
-public class MenuService extends MenuServiceHelper {
-    private final Scheduler jdbcScheduler;
+public class MenuService{
 
-    public static Logger LOG = LoggerFactory.getLogger(MenuService.class);
+    MenuServiceIntegration menuServiceIntegration;
 
-    public MenuService(@Qualifier("jdbcScheduler") Scheduler jdbcScheduler, MenuItemRepository menuItemRepository, AddOnRepository addOnRepository) {
-        super(menuItemRepository, addOnRepository);
-        this.jdbcScheduler = jdbcScheduler;
+    @Autowired
+    public MenuService(MenuServiceIntegration menuServiceIntegration)
+    {
+        this.menuServiceIntegration = menuServiceIntegration;
     }
 
-    private Mono<MenuItem> insertMenuItemToDatabase(MenuItem menuItem) {
-
-        return Mono.fromCallable(() -> insertMenuItem(menuItem)).subscribeOn(jdbcScheduler);
+    public Mono<MenuItemDto> addMenuItem(MenuItemDto menuItemDto) {
+        MenuItem menuItem = AppUtils.MenuUtils.dtoToEntity(menuItemDto);
+        return menuServiceIntegration.addMenuItem(menuItem);
     }
 
-    public ResponseEntity<Mono<MenuItem>> addMenuItem(AddMenuItemRequestBody addMenuItemRequestBody) {
-
-        try {
-            if (addMenuItemRequestBody.hasNullFields()) {
-                throw new BadRequestException();
-            }
-            MenuItem menuItem = AppUtils.MenuItemFromAddMenuItemRequestBody(addMenuItemRequestBody);
-            return new ResponseEntity<>(insertMenuItemToDatabase(menuItem), HttpStatus.CREATED);
-        } catch (BadRequestException e) {
-            LOG.error("Error saving menu item : Bad Request");
-            return ResponseEntity.badRequest().build();
-        } catch (Exception e) {
-            LOG.error("Error saving menu item");
-            return ResponseEntity.internalServerError().build();
-        }
-    }
-
-    private Flux<MenuItem> getAllMenuItemsFromDatabase() {
-        return Mono.fromCallable(menuItemRepository::findAll).flatMapMany(Flux::fromIterable).subscribeOn(jdbcScheduler);
-    }
-
-    public ResponseEntity<Flux<MenuItem>> getAllMenuItems() {
-        return ResponseEntity.ok(getAllMenuItemsFromDatabase());
+    public Flux<MenuItemDto> getAllMenuItems() {
+        return menuServiceIntegration.getAllMenuItems();
     }
 }
