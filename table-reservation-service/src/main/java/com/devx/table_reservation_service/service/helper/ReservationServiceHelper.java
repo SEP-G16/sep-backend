751f66ba8c45e753584da51a6b9955be35892c15
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-14 02:59:02 +0530

    new helper interface

---
+++ table-reservation-service/src/main/java/com/devx/table_reservation_service/service/helper/ReservationServiceHelper.java
@@ -0,0 +1,7 @@
+package com.devx.table_reservation_service.service.helper;
+
+import com.devx.table_reservation_service.model.RestaurantTable;
+
+public interface ReservationServiceHelper {
+    public RestaurantTable addTable(RestaurantTable restaurantTable);
+}
