3c2440621a4c4c63e0891d046a29f97164e5e88e
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-16 02:10:38 +0530

    cloudbuild with security config modified

--- api-gateway/src/main/java/com/devx/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/devx/api_gateway/security/SecurityConfig.java
@@ -72,7 +72,7 @@ class SecurityConfig {
         corsConfig.addAllowedMethod("PATCH");
         corsConfig.addAllowedMethod("POST");
         corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.setAllowedOriginPatterns(List.of("http://localhost:[*]"));
+        corsConfig.setAllowedOriginPatterns(List.of("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "User-Key", "Request-Tracker", "Session-Tracker", "X-XSRF-TOKEN", "X-IBM-CLIENT-ID", "Message-ID", "X-IBM-CLIENT-SECRET"));
         corsConfig.setExposedHeaders(Arrays.asList("X-Get-Header"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
