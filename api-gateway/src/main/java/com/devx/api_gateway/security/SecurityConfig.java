5a63185bee9022046a3340d4482e252f07d0c898
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-15 23:53:31 +0530

    put method allowed

--- api-gateway/src/main/java/com/devx/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/devx/api_gateway/security/SecurityConfig.java
@@ -68,6 +68,7 @@ class SecurityConfig {
         corsConfig.applyPermitDefaultValues();
         corsConfig.setAllowCredentials(true);
         corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("PUT");
         corsConfig.addAllowedMethod("PATCH");
         corsConfig.addAllowedMethod("POST");
         corsConfig.addAllowedMethod("OPTIONS");
