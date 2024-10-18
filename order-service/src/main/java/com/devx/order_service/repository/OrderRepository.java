d89d3624cf3092ade5944912d41b53f0352eba31
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-18 19:17:59 +0530

    added new method to get orders which have menu item and pending

--- order-service/src/main/java/com/devx/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/devx/order_service/repository/OrderRepository.java
@@ -1,10 +1,10 @@
 package com.devx.order_service.repository;
 
-import com.devx.order_service.enums.OrderStatus;
+import com.devx.order_service.enums.OrderItemStatus;
 import com.devx.order_service.model.Order;
-import com.devx.order_service.model.OrderItem;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +13,9 @@ import java.util.UUID;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findBySessionId(UUID uuid);
+
+    @Query("SELECT o FROM Order o JOIN o.orderItems oi WHERE oi.status = :status AND oi.menuItem.id = :menuItemId")
+    List<Order> findOrdersByMenuItemIdAndStatus(@Param("menuItemId") Long menuItemId, @Param("status") OrderItemStatus status);
+
 }
 
